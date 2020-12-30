99ec2ec58c7917bfa8be003c99909da39895d252
Meera Patel <patelmeeray@gmail.com>
2020-12-30 11:04:45 -0500

    update document

---
+++ R/data.R
@@ -0,0 +1,62 @@
+#' @title CONDITION_OCCURRENCE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 1205 rows and 5 variables:
+#' \describe{
+#'   \item{\code{person_id}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{gender}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{condition_start_date}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{condition_end_date}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{condition_source}}{character COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"CONDITION_OCCURRENCE" 
+
+#' @title DRUG_EXPOSURE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 10072 rows and 5 variables:
+#' \describe{
+#'   \item{\code{person_id}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{gender}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{drug_exposure_start_date}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{drug_exposure_end_date}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{drug_source}}{character COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"DRUG_EXPOSURE" 
+
+#' @title MEASUREMENT
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 614 rows and 4 variables:
+#' \describe{
+#'   \item{\code{person_id}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{gender}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{measurement_date}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{measurement_source}}{character COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"MEASUREMENT" 
+
+#' @title OBSERVATION
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 1378 rows and 4 variables:
+#' \describe{
+#'   \item{\code{person_id}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{gender}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{observation_date}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{observation_source}}{character COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"OBSERVATION" 
+
+#' @title PROCEDURE_OCCURRENCE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 3754 rows and 4 variables:
+#' \describe{
+#'   \item{\code{person_id}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{gender}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{procedure_date}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{procedure_source}}{character COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"PROCEDURE_OCCURRENCE" 
+
